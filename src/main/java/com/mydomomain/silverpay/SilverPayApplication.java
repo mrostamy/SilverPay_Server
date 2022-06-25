c57bf08efecca92963fa5c05736b99a8dabb6d8b
mrostamy <mrostamy92@gmail.com>
2022-06-25 16:14:47 +0430

    first init

---
+++ src/main/java/com/mydomomain/silverpay/SilverPayApplication.java
@@ -0,0 +1,13 @@
+package com.mydomomain.silverpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilverPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mydomomain/silverpay/SilverPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydomomain.silverpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilverPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
